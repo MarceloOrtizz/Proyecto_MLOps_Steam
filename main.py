cc5acdfc4a446ceba9dd0332cde3cbc945763833
Marcelo <marceloortizz@gmail.com>
2023-12-04 03:20:47 -0300

    corregir ruta gz en main

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def home():
 @app.get('/PlayTimeGenre/{genero}')
 def PlayTimeGenre(genero: str):  
   try:
-    consulta_1 = pd.read_csv('../data/consultas/PlayTimeGenre.csv.gz',compression='gzip')
+    consulta_1 = pd.read_csv('./data/consultas/PlayTimeGenre.csv.gz',compression='gzip')
     year_max =consulta_1[consulta_1['genres'].str.contains(genero)][['year','playtime_forever']].groupby('year').sum().idxmax().iloc[0]
     return year_max
   
@@ -25,7 +25,7 @@ def PlayTimeGenre(genero: str):
 @app.get('/UserForGenre/{genero}')
 def UserForGenre(genero: str):
   try:
-    consulta_2 = pd.read_csv('../data/consultas/UserForGenre.csv.gz',compression='gzip')
+    consulta_2 = pd.read_csv('./data/consultas/UserForGenre.csv.gz',compression='gzip')
     usuario=consulta_2[consulta_2['genres'].str.contains(genero)].sort_values('playtime_forever', ascending=False).iloc[0,1]
     consulta_2=consulta_2[['year','playtime_forever']][consulta_2['user_id']==usuario][consulta_2[['year','playtime_forever']][consulta_2['user_id']==usuario]['playtime_forever']>0.5]
     # paso el df a diccionario
