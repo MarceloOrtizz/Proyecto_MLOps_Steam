d2fe0dfd9c1a68e7288edd96a6d99814848f3446
Marcelo <marceloortizz@gmail.com>
2023-12-06 21:17:34 -0300

    ETL y testeos

--- main.py
+++ main.py
@@ -63,14 +63,15 @@ def UsersNotRecommend(year: int):
     return {f'ERROR: {e}'}
 
 @app.get('/sentiment_analysis/{año}')
-def sentiment_analysis(year):
+def sentiment_analysis(year: int):
   '''Cantidad de reseñas de usuarios para el año dado.'''
   try:
     consulta_5 = pd.read_csv('./data/consultas/sentiment_analysis.csv.gz',compression='gzip')
-    valores=consulta_5['sentiment_analysis'][consulta_5['year']==year].value_counts()
-    resultado = {'Negative': valores.get(0, 0),
-        'Neutral': valores.get(1, 0),
-        'Positive': valores.get(2, 0)}
+    valores=consulta_5['sentiment_analysis'][consulta_5['year']==year].value_counts().reset_index()
+    resultado = {'Negative': str(valores.loc[2, 'count']),
+        'Neutral': str(valores.loc[1, 'count']),
+        'Positive': str(valores.loc[0, 'count'])}
+    
     return resultado
 
   except Exception as e :
